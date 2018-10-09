74a8bb47ec27099f06e7c2920cdaae0fed64f015
Chris Walker <chris@thechriswalker.net>
2018-10-09 21:17:24 +0100

    Updates

---
+++ init/05-mononoki-nerd-font.sh
@@ -0,0 +1,14 @@
+# Rather than include the fonts, here we download and extract them from the website
+
+mkdir ~/.fonts
+getFont() {
+    local variant="${1}"
+    e_arrow "Installing font: Mononoki Nerd Font (${variant})"
+    curl \
+        --silent \
+	"https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Mononoki/${variant}/complete/mononoki-${variant}%20Nerd%20Font%20Complete%20Mono.ttf" \
+	> "~/.fonts/mononoki-${variant}%20Nerd%20Font%20Complete%20Mono.ttf"
+}
+
+getInputFont "Regular"
+getInputFont "Bold"
